e0a4b62e9a1a53c659e89e7aa7e1be0fffc01e72
Cogniven <yangtuohang@outlook.com>
2026-03-19 22:24:06 +0800

    update visual design page

---
+++ assets/images/works/visual-design/data.js
@@ -0,0 +1,23 @@
+const visualDesignWorks = [
+  {
+    slug: "work-001",
+    title: "Visual Work 001",
+    desc: "A short description for this piece.",
+    cover: "../../assets/images/works/visual-design/work-001/cover.jpg",
+    link: "#"
+  },
+  {
+    slug: "work-002",
+    title: "Visual Work 002",
+    desc: "Another short description for this piece.",
+    cover: "../../assets/images/works/visual-design/work-002/cover.jpg",
+    link: "#"
+  },
+  {
+    slug: "work-003",
+    title: "Visual Work 003",
+    desc: "This card is ready for future work.",
+    cover: "../../assets/images/works/visual-design/work-003/cover.jpg",
+    link: "#"
+  }
+];
