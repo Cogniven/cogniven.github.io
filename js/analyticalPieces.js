bee91065ef1936aaf518e360c0f54ec75595145e
Cogniven <yangtuohang@outlook.com>
2026-03-22 11:47:32 +0800

    update visual design page

--- js/analyticalPieces.js
+++ js/analyticalPieces.js
@@ -1,13 +1,43 @@
-{
-  title: {
-    en: "New Analytical Piece",
-    zh: "新的分析作品"
+const AnalyticalPieces = [
+  {
+    title: {
+      en: "New Analytical Piece",
+      zh: "新的分析作品"
+    },
+    desc: {
+      en: "A short description of the piece.",
+      zh: "这篇作品的简短说明。"
+    },
+    cover: "../../assets/images/works/analytical-pieces/work-001/cover.jpg",
+    link: "./work-001/index.html",
+    date: "2026-03-22"
   },
-  desc: {
-    en: "A short description of the piece.",
-    zh: "这篇作品的简短说明。"
+
+  {
+    title: {
+      en: "002",
+      zh: "002"
+    },
+    desc: {
+      en: "An exploration.",
+      zh: "探讨。"
+    },
+    cover: "../../assets/images/works/analytical-pieces/work-002/cover.jpg",
+    link: "./work-002/index.html",
+    date: "2026-03-18"
   },
-  cover: "../../assets/images/works/analytical-pieces/work-002/cover.jpg",
-  link: "./work-002/index.html",
-  date: "2026-03-22"
-}
+
+  {
+    title: {
+      en: "Liquidity Reversion",
+      zh: "流动性回归机制"
+    },
+    desc: {
+      en: "A structural analysis.",
+      zh: "机制分析。"
+    },
+    cover: "../../assets/images/works/analytical-pieces/work-003/cover.jpg",
+    link: "./work-003/index.html",
+    date: "2026-03-10"
+  }
+];
