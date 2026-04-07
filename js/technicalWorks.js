e6650b5bc279155dcafca30475543f3c6a96128e
Cogniven <yangtuohang@outlook.com>
2026-04-07 08:52:27 +0800

    update

--- js/technicalWorks.js
+++ js/technicalWorks.js
@@ -1,23 +1,23 @@
 const TechnicalWorks = [
   {
     title: {
-      en: "Strategy System",
-      zh: "策略系统"
+      en: "all-weather portfolio China market parameter optimization",
+      zh: "全天候投资组合中国市场参数优化",
     },
     desc: {
-      en: "A systematic trading strategy with structured entry, risk control, and optimization.",
-      zh: "一个具有结构化开仓、风控与优化逻辑的量化交易策略。"
+      en: "Parameter optimization for all-weather portfolio in the China market, forming a simple and effective investment strategy",
+      zh: "对全天候投资组合在中国市场的实践进行参数优化，形成简单而有效的投资策略",
     },
     type: {
-      en: "Strategy",
-      zh: "策略"
+      en: "Investment",
+      zh: "投资",
     },
     tools: {
-      en: "Python",
-      zh: "Python"
+      en: "Python,Excel",
+      zh: "Python,Excel",
     },
     cover: "/assets/images/works/technical-works/work-001/cover.jpg",
     link: "/assets/images/works/technical-works/work-001/index.html",
-    date: "2026-03-22"
-  }
+    date: "2026-03-22",
+  },
 ];
