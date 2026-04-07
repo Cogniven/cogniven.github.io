f4a2a4859f3abef651e60be8f2da59fb41599097
Cogniven <yangtuohang@outlook.com>
2026-04-07 08:36:52 +0800

    update

--- js/researchSystems.js
+++ js/researchSystems.js
@@ -1,23 +1,23 @@
 const ResearchSystems = [
   {
     title: {
-      en: "Framework",
-      zh: "研究框架"
+      en: "Quantitative Trading System",
+      zh: "量化交易研究",
     },
     desc: {
-      en: "A long-term research structure.",
-      zh: "一个长期研究结构。"
+      en: "A research approach to finding deterministic logic in a volatile market.",
+      zh: "一种在波动的市场中寻找确定逻辑的研究。",
     },
     category: {
-      en: "Finance",
-      zh: "财政"
+      en: "Investment",
+      zh: "投资",
     },
     status: {
       en: "Active",
-      zh: "进行中"
+      zh: "进行中",
     },
     cover: "/assets/images/projects/research-systems/project-001/cover.jpg",
     link: "/assets/images/projects/research-systems/project-001/index.html",
-    date: "2026-03-22"
-  }
+    date: "2026-03-22",
+  },
 ];
